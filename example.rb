e956d341543f42cffda1c96b5dbf0ad4367a050c
Jessica Shearwood <letsjesscode@gmail.com>
2020-06-08 13:32:08 +0000

    Automatically backed up by Learn

--- example.rb
+++ example.rb
@@ -1,5 +1,3 @@
-# Use this space to code along with the readme
-
 def say_hello_world_ten_times
   phrase = "Hello World!"
   puts phrase
@@ -13,6 +11,5 @@ def say_hello_world_ten_times
   puts phrase
   puts phrase
 end
- 
 
 say_hello_world_ten_times
